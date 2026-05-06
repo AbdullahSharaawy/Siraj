134a8ec69b2c7b1456ab267781789b1dc86aa7a9
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-06 22:53:28 +0300

    Create DonationBasicDto

---
+++ TheCharityBLL/DTOs/CampaignDTOs/DonationBasicDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCharityBLL.DTOs.CampaignDTOs
+{
+    public class DonationBasicDto
+    {
+        public int Id { get; set; }
+        public double Amount { get; set; }
+        public DateTime DonationDate { get; set; }
+        public string? DonorName { get; set; }
+    }
+}
