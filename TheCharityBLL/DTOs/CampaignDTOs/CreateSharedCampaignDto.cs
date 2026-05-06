380924891ab72d96b8c1d94a53ca551bce55ae2a
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-06 22:48:37 +0300

    Create CreateSharedCampaignDto

---
+++ TheCharityBLL/DTOs/CampaignDTOs/CreateSharedCampaignDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCharityBLL.DTOs.CampaignDTOs
+{
+    public class CreateSharedCampaignDto : CreateCampaignDto
+    {
+        public List<int> OrganizationIds { get; set; } = new List<int>();
+    }
+}
