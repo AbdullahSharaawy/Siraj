4f902e8759d16c13fbbdffe79928586583495f4f
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-07 00:43:12 +0300

    Create IncrementCampaignMoneyDto

---
+++ TheCharityBLL/DTOs/CampaignDTOs/IncrementCampaignMoneyDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCharityBLL.DTOs.CampaignDTOs
+{
+    public class IncrementCampaignMoneyDto
+    {
+        public int CampaignId { get; set; }
+        public double Amount { get; set; }
+    }
+}
