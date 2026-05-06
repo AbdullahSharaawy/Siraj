0d5e6b5a2919a02e30cdaab8f711bf5c4ced8fd2
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-07 00:05:54 +0300

    Create CreateOrganizationBasicDto

---
+++ TheCharityBLL/DTOs/CampaignDTOs/CreateOrganizationBasicDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCharityBLL.DTOs.CampaignDTOs
+{
+    public class CreateOrganizationBasicDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+}
