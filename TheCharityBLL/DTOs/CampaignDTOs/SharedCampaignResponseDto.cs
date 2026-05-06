f809b1553cb7b92a742aa857f3f9b81de471d7e2
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-06 22:46:00 +0300

    Clean up unused Imports in SharedCampaignResponseDto

--- TheCharityBLL/DTOs/CampaignDTOs/SharedCampaignResponseDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/SharedCampaignResponseDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TheCharityBLL.DTOs.CampaignDTOs
+﻿namespace TheCharityBLL.DTOs.CampaignDTOs
 {
     public class SharedCampaignResponseDto : CampaignResponseDto
     {
