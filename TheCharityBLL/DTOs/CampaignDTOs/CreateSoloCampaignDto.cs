1a128361d5884e94bc0a223200a11c0dd1f6c2c0
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-06 22:47:32 +0300

    Create CreateSoloCampaignDto

---
+++ TheCharityBLL/DTOs/CampaignDTOs/CreateSoloCampaignDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCharityBLL.DTOs.CampaignDTOs
+{
+    public class CreateSoloCampaignDto : CreateCampaignDto
+    {
+        public int OrganizationId { get; set; }
+    }
+}
