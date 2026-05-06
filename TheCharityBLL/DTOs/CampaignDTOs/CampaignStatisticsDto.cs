d9cc86163e2b84523acfc555c4b20513cf74b675
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-06 22:52:46 +0300

    Create CampaignStatisticsDto

---
+++ TheCharityBLL/DTOs/CampaignDTOs/CampaignStatisticsDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCharityBLL.DTOs.CampaignDTOs
+{
+    public class CampaignStatisticsDto
+    {
+        public int TotalCampaigns { get; set; }
+        public int ActiveCampaigns { get; set; }
+        public int CompletedCampaigns { get; set; }
+        public int PendingCampaigns { get; set; }
+        public int CancelledCampaigns { get; set; }
+        public double TotalMoneyRaised { get; set; }
+        public double AverageAchievementPercentage { get; set; }
+        public int SoloCampaignsCount { get; set; }
+        public int SharedCampaignsCount { get; set; }
+        public CampaignResponseDto? MostSuccessfulCampaign { get; set; }
+        public CampaignResponseDto? MostDonatedCampaign { get; set; }
+        public DateTime StatisticsDate { get; set; }
+    }
+}
