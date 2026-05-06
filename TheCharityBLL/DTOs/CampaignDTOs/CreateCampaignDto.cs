a7f4455d7442165107a19675727694559da9736d
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-06 22:46:40 +0300

    Create CreateCampaignDto

---
+++ TheCharityBLL/DTOs/CampaignDTOs/CreateCampaignDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheCharityDAL.Enums;
+
+namespace TheCharityBLL.DTOs.CampaignDTOs
+{
+    public class CreateCampaignDto
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public double Target { get; set; }
+        public CampaignType Type { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
