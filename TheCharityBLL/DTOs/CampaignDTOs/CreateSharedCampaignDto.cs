75f218b1dfd9a5353886f8e74f97e492997158d6
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-07 00:56:50 +0300

    update Campaigns DTOs

--- TheCharityBLL/DTOs/CampaignDTOs/CreateSharedCampaignDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/CreateSharedCampaignDto.cs
@@ -3,5 +3,7 @@
     public class CreateSharedCampaignDto : CreateCampaignDto
     {
         public List<int> OrganizationIds { get; set; } = new List<int>();
+        public string? ImgPath { get; set; }
+        public int? Target { get; set; }
     }
 }
--- TheCharityBLL/DTOs/CampaignDTOs/CreateSoloCampaignDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/CreateSoloCampaignDto.cs
@@ -3,5 +3,7 @@
     public class CreateSoloCampaignDto : CreateCampaignDto
     {
         public int OrganizationId { get; set; }
+        public string? ImgPath { get; set; }
+        public int? Target { get; set; }
     }
 }
