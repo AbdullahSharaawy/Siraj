82420d8c86e6c4332da0317be56522fedf142aba
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-06 22:51:53 +0300

    Create CampaignValidationDto

---
+++ TheCharityBLL/DTOs/CampaignDTOs/CampaignValidationDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCharityBLL.DTOs.CampaignDTOs
+{
+    public class CampaignValidationDto
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public double? Target { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? OrganizationId { get; set; }
+    }
+}
