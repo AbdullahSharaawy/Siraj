faf0bcd1728409a20611e236cf1ef3a4d25c4ece
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-06 22:49:21 +0300

    Create UpdateCampaignDto

---
+++ TheCharityBLL/DTOs/CampaignDTOs/UpdateCampaignDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCharityBLL.DTOs.CampaignDTOs
+{
+    public class UpdateCampaignDto
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public double? Target { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
