b4b982c0f77c9b8770ff3f52bab58fd6c0342075
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-06 23:52:22 +0300

    update CampaignResponseDto attriputes

--- TheCharityBLL/DTOs/CampaignDTOs/CampaignResponseDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/CampaignResponseDto.cs
@@ -7,14 +7,13 @@ namespace TheCharityBLL.DTOs.CampaignDTOs
         public int Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
+        public string? ImgPath { get; set; }
         public double? Target { get; set; }
         public double? Achieved { get; set; }
-        public CampaignStatus Status { get; set; }
-        public CampaignType Type { get; set; }
-        public DateTime RegistrationDate { get; set; }
-        public DateTime? StartDate { get; set; }
-        public DateTime? EndDate { get; set; }
-        public double AchievementPercentage { get; set; }
+        public CampaignStatus? Status { get; set; }
+        public CampaignType? Type { get; set; }
         public bool IsDeleted { get; set; }
+        public DateTime? RegistrationDate { get; set; }
+        public DateTime? UpdatedOn { get; set; }
     }
 }
