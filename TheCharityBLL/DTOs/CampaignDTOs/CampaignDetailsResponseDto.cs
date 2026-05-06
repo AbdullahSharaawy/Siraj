9085b0e2b66b37f491390f30ef351a37f238113d
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-06 22:43:20 +0300

    Create CampaignDetailsResponseDto

---
+++ TheCharityBLL/DTOs/CampaignDTOs/CampaignDetailsResponseDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCharityBLL.DTOs.CampaignDTOs
+{
+    public class CampaignDetailsResponseDto : CampaignResponseDto
+    {
+        public List<DonationBasicDto>? RecentDonations { get; set; }
+        public int TotalDonationsCount { get; set; }
+        public double RemainingAmount { get; set; }
+        public int DaysRemaining { get; set; }
+    }
+}
