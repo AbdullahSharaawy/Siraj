72d0852f98f9858b7a312035b767f32a7d9a2e72
csgoat0 <elturkabdelrahman@gmain.com>
2026-05-06 22:45:39 +0300

    Create SharedCampaignResponseDto

---
+++ TheCharityBLL/DTOs/CampaignDTOs/SharedCampaignResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheCharityBLL.DTOs.CampaignDTOs
+{
+    public class SharedCampaignResponseDto : CampaignResponseDto
+    {
+        public List<OrganizationBasicDto>? Organizations { get; set; }
+        public int OrganizationsCount { get; set; }
+    }
+}
