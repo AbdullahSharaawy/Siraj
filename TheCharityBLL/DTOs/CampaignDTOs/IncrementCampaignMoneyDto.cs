155b319737ed89c6e7ab87c4fdf9ef972349abcf
CSGoat0 <elturkabdelrahman@gmail.com>
2026-05-15 15:28:32 +0300

    adding examples to IncrementCampaignMoneyDto

--- TheCharityBLL/DTOs/CampaignDTOs/IncrementCampaignMoneyDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/IncrementCampaignMoneyDto.cs
@@ -2,7 +2,9 @@
 {
     public class IncrementCampaignMoneyDto
     {
+        /// <example>1</example>
         public int CampaignId { get; set; }
+        /// <example>500</example>
         public double Amount { get; set; }
     }
 }
