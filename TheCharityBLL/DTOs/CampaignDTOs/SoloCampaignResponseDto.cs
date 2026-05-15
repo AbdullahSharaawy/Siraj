b1c691cf4354f2e23774b0acadce7ae8e7ba2177
CSGoat0 <elturkabdelrahman@gmail.com>
2026-05-15 15:29:57 +0300

    adding examples to SoloCampaignResponseDto

--- TheCharityBLL/DTOs/CampaignDTOs/SoloCampaignResponseDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/SoloCampaignResponseDto.cs
@@ -2,7 +2,9 @@
 {
     public class SoloCampaignResponseDto : CampaignResponseDto
     {
+        /// <example>5</example>
         public int? OrganizationId { get; set; }
+        /// <example>Helping Hands NGO</example>
         public string? OrganizationName { get; set; }
     }
 }
