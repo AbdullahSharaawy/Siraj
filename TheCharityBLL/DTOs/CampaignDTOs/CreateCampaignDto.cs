42e41b27239318ed072ae1f8fc06af3f105c76ab
CSGoat0 <elturkabdelrahman@gmail.com>
2026-05-15 15:26:10 +0300

    adding examples to CreateCampaignDto

--- TheCharityBLL/DTOs/CampaignDTOs/CreateCampaignDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/CreateCampaignDto.cs
@@ -4,11 +4,17 @@ namespace TheCharityBLL.DTOs.CampaignDTOs
 {
     public class CreateCampaignDto
     {
+        /// <example>Help the Children</example>
         public string? Title { get; set; }
+        /// <example>Providing education for underprivileged children</example>
         public string? Description { get; set; }
+        /// <example>50000</example>
         public double Target { get; set; }
+        /// <example>Solo</example>
         public CampaignType Type { get; set; }
+        /// <example>2024-03-01T00:00:00</example>
         public DateTime? StartDate { get; set; }
+        /// <example>2024-05-01T00:00:00</example>
         public DateTime? EndDate { get; set; }
     }
 }
