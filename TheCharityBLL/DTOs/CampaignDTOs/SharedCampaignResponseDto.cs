5433a16e1e76b2bcd3c4286f0a4c6712b4e98b97
CSGoat0 <elturkabdelrahman@gmail.com>
2026-05-15 15:29:30 +0300

    adding examples to SharedCampaignResponseDto

--- TheCharityBLL/DTOs/CampaignDTOs/SharedCampaignResponseDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/SharedCampaignResponseDto.cs
@@ -3,6 +3,7 @@
     public class SharedCampaignResponseDto : CampaignResponseDto
     {
         public List<OrganizationBasicDto>? Organizations { get; set; }
+        /// <example>3</example>
         public int OrganizationsCount { get; set; }
     }
 }
