097c72168fd157239f01b88c8f61967cb943a76e
CSGoat0 <elturkabdelrahman@gmail.com>
2026-05-15 15:26:53 +0300

    adding examples to CreateSharedCampaignDto

--- TheCharityBLL/DTOs/CampaignDTOs/CreateSharedCampaignDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/CreateSharedCampaignDto.cs
@@ -2,8 +2,11 @@
 {
     public class CreateSharedCampaignDto : CreateCampaignDto
     {
+        /// <example>[1, 2, 3]</example>
         public List<int> OrganizationIds { get; set; } = new List<int>();
+        /// <example>/images/shared_campaign.jpg</example>
         public string? ImgPath { get; set; }
+        /// <example>100000</example>
         public int? Target { get; set; }
     }
 }
