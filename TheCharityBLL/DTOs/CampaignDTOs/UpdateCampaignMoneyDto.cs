f0a6cb5303913bfa86a39bee6c6e887e2b7063d6
CSGoat0 <elturkabdelrahman@gmail.com>
2026-05-15 15:31:12 +0300

    adding examples to UpdateCampaignMoneyDto

--- TheCharityBLL/DTOs/CampaignDTOs/UpdateCampaignMoneyDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/UpdateCampaignMoneyDto.cs
@@ -2,7 +2,9 @@
 {
     public class UpdateCampaignMoneyDto
     {
+        /// <example>1</example>
         public int CampaignId { get; set; }
+        /// <example>1000</example>
         public double Amount { get; set; }
     }
 }
