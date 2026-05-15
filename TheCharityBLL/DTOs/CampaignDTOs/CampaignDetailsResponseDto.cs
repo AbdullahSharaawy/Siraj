730907ef09621055c475c859491fb91d6629eef2
CSGoat0 <elturkabdelrahman@gmail.com>
2026-05-15 15:24:12 +0300

    adding examples to CampaginDetailsResponseDto, temporarly disable google and facebook auth, update appsettings, delete un-used dto.

--- TheCharityBLL/DTOs/CampaignDTOs/CampaignDetailsResponseDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/CampaignDetailsResponseDto.cs
@@ -2,8 +2,11 @@
 {
     public class CampaignDetailsResponseDto : CampaignResponseDto
     {
+        /// <example>75.5</example>
         public double AchievementPercentage { get; set; }
+        /// <example>25000</example>
         public double RemainingAmount { get; set; }
+        /// <example>42</example>
         public int TotalDonationsCount { get; set; }
         public List<DonationBasicDto>? RecentDonations { get; set; }
     }
--- TheCharityBLL/DTOs/CampaignDTOs/CreateOrganizationBasicDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TheCharityBLL.DTOs.CampaignDTOs
-{
-    public class CreateOrganizationBasicDto
-    {
-        public int Id { get; set; }
-        public string? Name { get; set; }
-    }
-}
