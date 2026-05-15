222a6913fbc5e949dcf5663fbb83c46ede4366a4
CSGoat0 <elturkabdelrahman@gmail.com>
2026-05-15 15:27:25 +0300

    adding examples to CreateSoloCampaignDto

--- TheCharityBLL/DTOs/CampaignDTOs/CreateSoloCampaignDto.cs
+++ TheCharityBLL/DTOs/CampaignDTOs/CreateSoloCampaignDto.cs
@@ -2,8 +2,11 @@
 {
     public class CreateSoloCampaignDto : CreateCampaignDto
     {
+        /// <example>5</example>
         public int OrganizationId { get; set; }
+        /// <example>/images/solo_campaign.jpg</example>
         public string? ImgPath { get; set; }
+        /// <example>50000</example>
         public int? Target { get; set; }
     }
 }
