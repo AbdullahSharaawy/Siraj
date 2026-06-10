e2a1b98bd6c77b6c1b20cde21b3599a64e5974f5
CSGoat0 <elturkabdelrahman@gmail.com>
2026-06-10 14:59:12 +0300

    Create CampaignDeadlineExtendedEventHandler

---
+++ TheCharityBLL/Events/EventHandlers/CampaignEventHandlers/CampaignDeadlineExtendedEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using TheCharityBLL.Events.Abstraction;
+using TheCharityBLL.Events.CampaignEvents;
+using TheCharityBLL.Services.Abstraction;
+
+namespace TheCharityBLL.Events.EventHandlers.CampaignEventHandlers
+{
+    public class CampaignDeadlineExtendedEventHandler : IEventHandler<CampaignDeadlineExtendedEvent>
+    {
+        private readonly IJobSchedulerService _jobScheduler;
+
+        public CampaignDeadlineExtendedEventHandler(IJobSchedulerService jobScheduler)
+        {
+            _jobScheduler = jobScheduler;
+        }
+
+        public async Task HandleAsync(CampaignDeadlineExtendedEvent @event)
+        {
+            _jobScheduler.EnqueueJob<DeadlineExtensionConfirmationJob>(new { CampaignId = @event.Campaign.Id });
+
+            await Task.CompletedTask;
+        }
+    }
+}
