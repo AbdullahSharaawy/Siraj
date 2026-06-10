931215c7f46e81d68d16d903e3b4248e2d0368f6
CSGoat0 <elturkabdelrahman@gmail.com>
2026-06-10 14:46:48 +0300

    Create CampaignDonationReceivedEvent

---
+++ TheCharityBLL/Events/CampaignEvents/CampaignDonationReceivedEvent.cs
@@ -0,0 +1,10 @@
+﻿using TheCharityDAL.Entities;
+
+namespace TheCharityBLL.Events.CampaignEvents
+{
+    public class CampaignDonationReceivedEvent
+    {
+        public Campaign Campaign { get; set; } = null!;
+        public double Amount { get; set; }
+    }
+}
