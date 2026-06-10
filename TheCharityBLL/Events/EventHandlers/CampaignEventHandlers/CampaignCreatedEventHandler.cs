02cf8c9f49f3dab400fde18eb1c3de84a6265afd
CSGoat0 <elturkabdelrahman@gmail.com>
2026-06-10 14:58:30 +0300

    Create CampaignCreatedEventHandler

---
+++ TheCharityBLL/Events/EventHandlers/CampaignEventHandlers/CampaignCreatedEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using TheCharityBLL.Events.Abstraction;
+using TheCharityBLL.Events.CampaignEvents;
+using TheCharityBLL.Services.Abstraction;
+
+namespace TheCharityBLL.Events.EventHandlers.CampaignEventHandlers
+{
+    public class CampaignCreatedEventHandler : IEventHandler<CampaignCreatedEvent>
+    {
+        private readonly IJobSchedulerService _jobScheduler;
+
+        public CampaignCreatedEventHandler(IJobSchedulerService jobScheduler)
+        {
+            _jobScheduler = jobScheduler;
+        }
+
+        public async Task HandleAsync(CampaignCreatedEvent @event)
+        {
+            _jobScheduler.EnqueueJob<NewCampaignNotificationJob>(new { CampaignId = @event.Campaign.Id });
+
+            await Task.CompletedTask;
+        }
+    }
+}
