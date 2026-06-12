fc7cbaa339980af7796e7e305bae6d1eae0b3641
CSGoat0 <elturkabdelrahman@gmail.com>
2026-06-12 23:49:17 +0300

    Fix OrgId = Null on a Campaign in Database

--- TheCharityDAL/Entities/SoloCampaign.cs
+++ TheCharityDAL/Entities/SoloCampaign.cs
@@ -5,7 +5,6 @@ namespace TheCharityDAL.Entities
 {
     public class SoloCampaign: Campaign
     {
-        public override int? OrganizationId { get; protected set; }
         public Organization? Organization { get; private set; }
         public SoloCampaign(string? title, string? description, string? imgPath, int? target, int? achieved, CampaignStatus? status, CampaignType? type, int? organizationId, DateTime deadline) : base(title, description, imgPath, target, achieved, status, type, deadline)
         {
