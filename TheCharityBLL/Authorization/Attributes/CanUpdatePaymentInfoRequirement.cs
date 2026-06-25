d2633105f6a9fffcc97490ef0d52a1375ed81464
CSGoat0 <elturkabdelrahman@gmail.com>
2026-06-25 22:36:04 +0300

    Create CanManageSubAdminsRequirement & CanUpdatePaymentInfoRequirement

---
+++ TheCharityBLL/Authorization/Attributes/CanUpdatePaymentInfoRequirement.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace TheCharityBLL.Authorization.Attributes
+{
+    public class CanUpdatePaymentInfoRequirement : IAuthorizationRequirement
+    {
+    }
+}
