0043d09ee771683f90d46ce6b8ee46fd7b4ebb5c
CSGoat0 <elturkabdelrahman@gmail.com>
2026-06-26 12:21:49 +0300

    Create CanCreateCampaignAttribute

---
+++ TheCharityBLL/Authorization/Attributes/CanCreateCampaignAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace TheCharityBLL.Authorization.Attributes
+{
+    /// <summary>
+    /// Allows access to users who can create campaigns for the specified organization.
+    /// This includes SuperAdmin, OrganizationAdmin, and SubAdmin.
+    /// </summary>
+    public class CanCreateCampaignAttribute : AuthorizeAttribute
+    {
+        public CanCreateCampaignAttribute()
+        {
+            Policy = "CanCreateCampaign";
+        }
+    }
+}
