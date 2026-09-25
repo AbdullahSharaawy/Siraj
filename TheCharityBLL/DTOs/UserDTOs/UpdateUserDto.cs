42e9c3909a848264402805bd7a4fc9bfc82144cf
AbdullahSharaawy <abdallahsharawy200@gmail.com>
2026-09-25 23:21:36 +0300

    added full name field

--- TheCharityBLL/DTOs/UserDTOs/UpdateUserDto.cs
+++ TheCharityBLL/DTOs/UserDTOs/UpdateUserDto.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +15,6 @@ namespace TheCharityBLL.DTOs.UserDTOs
         public string? Email { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
+        public string? FullName { get; set; }
     }
 }
